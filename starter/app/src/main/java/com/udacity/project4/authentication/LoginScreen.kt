984fcdf1b0c94de81aa45cda5d9a06237ff86ccb
Antony <vontonnie@gmail.com>
2021-02-28 20:26:17 +0300

    Update Kotlin Version

--- starter/app/src/main/java/com/udacity/project4/authentication/LoginScreen.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/LoginScreen.kt
@@ -6,11 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.registerForActivityResult
+import androidx.fragment.app.viewModels
 import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
+import timber.log.Timber
 
 
 class LoginScreen : Fragment() {
+
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.authStateLiveData.observe(viewLifecycleOwner){
+            
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,11 +31,10 @@ class LoginScreen : Fragment() {
     ): View? {
 
 
+        val authResult = registerForActivityResult(AuthResultContract()) {
 
-val authResult = registerForActivityResult(AuthResultContract()){
-
-    handleResponse(it)
-}
+            handleResponse(it)
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login_screen, container, false)
     }
@@ -30,12 +42,11 @@ val authResult = registerForActivityResult(AuthResultContract()){
     private fun handleResponse(idpResponse: IdpResponse?) {
 
 
-         if (idpResponse == null || idpResponse.error!=null){
+        if (idpResponse == null || idpResponse.error != null) {
 
-
-         }
-        else{
-
+            Timber.i("Login Unsuccessful")
+        } else {
+            Timber.i("Login Successful")
         }
     }
 
