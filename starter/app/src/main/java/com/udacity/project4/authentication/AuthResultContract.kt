678a2e50a95c4dad203d68c71123a35be3336fce
Antony <vontonnie@gmail.com>
2021-02-28 19:40:23 +0300

    Add Timber Lib

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthResultContract.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthResultContract.kt
@@ -1,4 +1,55 @@
 package com.udacity.project4.authentication
 
-class AuthResultContract {
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.udacity.project4.R
+
+
+/*sending an Int to NextActivity through an intent in the method createIntent
+ and expecting to get a result (IdpResponse) with parseResult function.*/
+
+class AuthResultContract : ActivityResultContract<Int, IdpResponse>() {
+
+    private val providers =
+        listOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(), AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+    companion object {
+
+        const val INPUT_KEY = "input_key"
+    }
+
+    /*create an intent to send an Input Int*/
+    override fun createIntent(context: Context, input: Int?): Intent {
+
+        return AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.drawable.ic_location)
+                .setTheme(R.style.AppTheme)
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(true)
+                .build()
+                .apply {
+                    putExtra(INPUT_KEY, input)
+                }
+
+
+    }
+
+    /*Convert result obtained from Activity.onActivityResult(int, int, Intent) to O*/
+    override fun parseResult(resultCode: Int, intent: Intent?): IdpResponse? {
+
+        return when (resultCode) {
+
+            Activity.RESULT_OK -> IdpResponse.fromResultIntent(intent)
+            else -> null
+
+        }
+
+    }
 }
--- starter/app/src/main/java/com/udacity/project4/authentication/LoginScreen.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/LoginScreen.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.registerForActivityResult
+import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
 
 
@@ -17,9 +19,24 @@ class LoginScreen : Fragment() {
 
 
 
+val authResult = registerForActivityResult(AuthResultContract()){
 
+    handleResponse(it)
+}
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login_screen, container, false)
+    }
+
+    private fun handleResponse(idpResponse: IdpResponse?) {
+
+
+         if (idpResponse == null || idpResponse.error!=null){
+
+
+         }
+        else{
+
+        }
     }
 
 }
