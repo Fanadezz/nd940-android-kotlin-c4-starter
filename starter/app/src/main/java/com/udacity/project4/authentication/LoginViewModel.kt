3a804071110111cb3d4c98b4172aa291e1dd1371
Antony <vontonnie@gmail.com>
2021-02-28 18:23:22 +0300

    Add LoginViewModel

--- starter/app/src/main/java/com/udacity/project4/authentication/LoginViewModel.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/LoginViewModel.kt
@@ -1,4 +1,27 @@
 package com.udacity.project4.authentication
 
-class LoginViewModel {
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+
+class LoginViewModel: ViewModel() {
+
+
+    enum class AuthStateEnum{
+
+        AUTHENTICATED, UNAUTHENTICATED
+    }
+
+
+    val authStateLiveData = FirebaseUserLiveData().map {
+        if (it != null) {
+            AuthStateEnum.AUTHENTICATED
+        }
+        else{
+
+            AuthStateEnum.UNAUTHENTICATED
+        }
+    }
+
 }
