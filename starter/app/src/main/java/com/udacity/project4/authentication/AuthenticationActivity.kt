744edf49f879d5f26cf7e3c210e392ea0fe47849
Antony <vontonnie@gmail.com>
2021-03-05 07:10:24 +0300

    Add Login Button

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,11 +1,15 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 import timber.log.Timber
 
 /**
@@ -24,10 +28,33 @@ class AuthenticationActivity : AppCompatActivity() {
         const val SIGN_IN_RESULT_CODE = 1001
     }
 
+    //register Activity Result API
+    private val authResultLauncher = registerForActivityResult(AuthResultContract()){
+
+        handleResponse(it)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        viewModel.authStateLiveData.observe(this){
+
+            when(it){
+                //navigate to RemindersActivity when Authenticated
+                LoginViewModel.AuthStateEnum.AUTHENTICATED -> {
+
+
+                    startActivity(Intent(this, RemindersActivity::class.java))
+
+                }
+                //prompt login if unauthenticated
+                else->{
+
+                    authResultLauncher.launch(SIGN_IN_RESULT_CODE)
+                }
+            }
+        }
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
 
